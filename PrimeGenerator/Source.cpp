80b250fcc3558f5c3056e4724bce570b0280edcf
Stelless <47225505+Stelless@users.noreply.github.com>
2023-07-26 16:10:17 -0700

    Changed functionality to calculate up to the nth prime. Instead of all primes under n.

--- PrimeGenerator/Source.cpp
+++ PrimeGenerator/Source.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+#include <chrono>
 #define dbgline(x) std::cout << #x << ": " << (x) << std::endl;
 using namespace std;
+
+/*
+I want to compare this method with a seive method. This is brute force checking each number to see if it is divisible by a prime we've already catpured. If not it's a new prime and we capture it
+
+*/
+
 
 int main() {
    std::vector<int> primes;
    primes.push_back(2);//Seeding with the first prime
    int index = 3;
 
-   while (index < 1000) {
+   const auto start = std::chrono::steady_clock::now();
+
+   while (primes.size() < 100000) {
       int prime_count = primes.size();
       bool composite = false;
-      for (unsigned int i = 0; i < prime_count; i++) {
+      for (unsigned int i = 1; i < prime_count; i++) {
          if (index % primes[i] == 0) {
             composite = true;
             break;
@@ -25,13 +33,19 @@ int main() {
       index += 2;
    }
 
+   const auto end = std::chrono::steady_clock::now();
+   const std::chrono::duration<double> elapsed_seconds = end - start;
+
    cout << "PRIME COUNT : " << primes.size() << endl;
+   cout << "DURATION: " << elapsed_seconds.count() << endl;
+
+   cout << "Writing to file..." << endl;
    std::ofstream outputfile;
    outputfile.open("primes.txt");
 
    for (const auto& prime : primes) {
-      cout << prime << ", ";
       outputfile << prime << "\n";
    }
+
    outputfile.close();
 }
